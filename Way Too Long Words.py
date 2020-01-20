0a8769f554188090e3a3edd5ad5b29b5c9f35f1a
f4him <shahriarfahim23@gmail.com>
2020-01-21 03:03:11 +0600

    update

--- Way Too Long Words.py
+++
@@ -1,8 +0,0 @@
-m=int(input())
-for x in range(m):
-    s=str(input())
-    if len(s)>10:
-        size=len(s)
-        print(s[0],size-2,s[size-1], sep='')
-    else:
-        print(s)
--- theatre.py
+++
@@ -1,6 +0,0 @@
-import math
-m, n, a = input().split()
-m, n, a = [int(m), int(n), int(a)]
-p=math.ceil(m/a)
-q=math.ceil(n/a)
-print(p*q)
